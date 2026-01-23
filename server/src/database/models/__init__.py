c48e81caf52da7fbc954d2d92cf76c51cdd7e0ff
Swapnil Dhamal <swapnildhamal58@gmail.com>
2026-01-23 14:18:01 +0530

    updates in session and db

--- server/src/database/models/__init__.py
+++ server/src/database/models/__init__.py
@@ -1,4 +1,4 @@
 # app/db/models/__init__.py
 from .user import User
-from .audio import Audio
-from .analysis import AnalysisRequest, AnalysisReport, AudioAnalysis, AudioMetadata
+# from .audio import Audio
+# from .analysis import AnalysisRequest, AnalysisReport, AudioAnalysis, AudioMetadata
--- server/src/database/session.py
+++ server/src/database/session.py
@@ -1,10 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from dotenv import load_dotenv
-from ..core.settings import CONSTANTS
+from src.core.settings import CONSTANTS
 
-DATABASE_URL: str = f"postgresql+asyncpg://{CONSTANTS.DATABASE_USER}:{CONSTANTS.DATABASE_PASSWORD}@{CONSTANTS.DATABASE_HOST}:{CONSTANTS.DATABASE_PORT}/{CONSTANTS.DATABASE_NAME}"
-async_engine = create_async_engine(DATABASE_URL)
+async_engine = create_async_engine(CONSTANTS.ASYNC_DATABASE_URL)
 AsyncSessionLocal = async_sessionmaker(bind=async_engine, expire_on_commit=False)
 
 async def get_db():

d5b051f76b3845535f3211e7820ffd58e692002f
712Kunal <jadhavkun0@gmail.com>
2026-01-26 14:22:00 +0530

    Added some components

---
+++ client/src/shared/pages/AccessDenied.page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AccessDeniedPage() {
+  return <div>AccessDenied.page</div>;
+}
+
+export default AccessDeniedPage;
---
+++ client/src/shared/pages/NotFound.page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function NotFoundPage() {
+  return <div>NotFound.page</div>;
+}
+
+export default NotFoundPage;

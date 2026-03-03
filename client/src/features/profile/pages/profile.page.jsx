322bf0bd73d911b03778c98bcdf37958adcf243b
712Kunal <jadhavkun0@gmail.com>
2026-03-03 16:00:16 +0530

    added the reset password moodle

--- client/src/features/profile/pages/profile.page.jsx
+++ client/src/features/profile/pages/profile.page.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { React, useState } from "react";
 import { Button } from "@/components/ui/button";
 import useUserAuthFlow from "@/features/auth/flows/userAuth.flow";
 import { useDispatch } from "react-redux";
@@ -6,7 +6,10 @@ import { toast } from "react-toastify";
 
 import { setAuthStep } from "../../auth/state/slices/auth.slice";
 
+import { ResetPasswordDialog } from "@/features/auth/components/ResetPassword";
+
 function ProfilePage() {
+  const [isResetDialogOpen, setIsResetDialogOpen] = useState(false);
   const dispatch = useDispatch();
   const { flow } = useUserAuthFlow();
 
@@ -35,6 +38,18 @@ function ProfilePage() {
     <div className="flex flex-col">
       <h1>WELCOME USER</h1>
       <Button onClick={handleLogout}>LOGOUT</Button>
+
+      <Button
+        onClick={() => setIsResetDialogOpen(true)}
+        className="px-6 py-2 mt-10"
+      >
+        Open Reset Password Dialog
+      </Button>
+
+      <ResetPasswordDialog
+        open={isResetDialogOpen}
+        onOpenChange={setIsResetDialogOpen}
+      />
     </div>
   );
 }

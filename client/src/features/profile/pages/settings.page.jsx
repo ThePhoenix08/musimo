0065e969b023d3f4c51bdfff3af52edd036a44fd
Drkadam07 <drkadam04@gmail.com>
2026-05-19 18:23:06 +0530

    ADD: enhance SettingsPage layout with structured components for better user experience

--- client/src/features/profile/pages/settings.page.jsx
+++ client/src/features/profile/pages/settings.page.jsx
@@ -1,4 +1,21 @@
+import AccountSettings from "../components/account-settings";
+import DangerZone from "../components/danger-zone";
+import PreferencesSettings from "../components/preferences-settings";
+import SubscriptionCard from "../components/subscription-card";
+
 function SettingsPage() {
-  return <div>SettingsPage</div>;
+  return (
+    <div className="h-screen overflow-y-auto px-6 py-6">
+      <h2 className="text-2xl font-bold">
+         Settings
+      </h2>
+      <div className="mt-6 space-y-6">
+        <PreferencesSettings />
+        <SubscriptionCard />
+        <DangerZone />
+      </div>
+    </div>
+  );
 }
+
 export default SettingsPage;
